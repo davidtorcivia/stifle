7c32dbf1d86682ae19f2309c092f1dcb061bbfc1
David Torcivia <david.torcivia@gmail.com>
2025-12-19 16:39:28 -0500

    Version: 0.2.0-alpha

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "app.stifle"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "0.2.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
